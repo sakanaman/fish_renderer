b15b952f684372caff0bfe351a5c4fa92b825f37
sakanaman <sakana1231@icloud.com>
2019-11-12 16:13:49 +0900

    little fix in include path

--- src/Parallel/concurrent.hpp
+++ src/Parallel/concurrent.hpp
@@ -1,6 +1,29 @@
-#include "concurrent.hpp"
+#ifndef CONCURRENT_HPP
+#define CONCURRENT_HPP
 
-ParallelRender::ParallelRender(const std::function<void(int*, int*)>& _render):count(0),render(_render){}
+#include <vector>
+#include <functional>
+#include <mutex>
+#include <atomic>
+#include <thread>
+#include <iostream>
+#include "random.hpp"
+
+//task allocate with tile splitt
+class ParallelRender
+{
+public:
+    ParallelRender(const std::function<void(const int*,const int*, pcg32_random_t*)>& _render);
+    void Execute(const int width, const int height, const int split_num);
+private:
+    std::atomic<int> count;
+    std::mutex mut;
+    std::function<void(const int*, const int*, pcg32_random_t*)> render;
+    // --> render function argument = (upper_left[2], bottom_right[2], rng_ptr)
+};
+
+//
+ParallelRender::ParallelRender(const std::function<void(const int*, const int*, pcg32_random_t*)>& _render):count(0),render(_render){}
 
 
 void ParallelRender::Execute(const int width, const int height, const int split_num)
@@ -22,7 +45,7 @@ void ParallelRender::Execute(const int width, const int height, const int split_
     // j|
     //  |
     auto renderthread = 
-        [&]()
+        [&](pcg32_random_t* rng)
         {
             int upper_left[2] = {-1, -1};
             int bottom_right[2] = {-1, -1};
@@ -47,16 +70,21 @@ void ParallelRender::Execute(const int width, const int height, const int split_
                     }
                 }
 
-                render(upper_left, bottom_right);
+                render(upper_left, bottom_right, rng);
             }
         };
 
     int thread_num = std::thread::hardware_concurrency();
+    std::vector<pcg32_random_t> rngs(thread_num);
+    for(int i = 0; i < thread_num; ++i)
+    {
+        initRNG(&(rngs[i]));
+    }
 
     std::vector<std::thread> threads(thread_num);
     for(int i = 0; i < thread_num; ++i)
     {
-        threads[i] = std::thread(renderthread);
+        threads[i] = std::thread(renderthread, &(rngs[i]));
     }
 
     for(int i = 0; i < thread_num; i++)
@@ -66,3 +94,6 @@ void ParallelRender::Execute(const int width, const int height, const int split_
 
     std::cout << count << std::endl;
 }
+//
+
+#endif
